42cd4259342b306a2c86f550328296170faaa162
Rahmatilla Xudoyberdiyev <xudoyberdiyevrahmatilla@gmail.com>
2023-12-27 10:24:48 +0500

    Some changes

--- src/components/movie-list-item/MovieListItem.vue
+++ src/components/movie-list-item/MovieListItem.vue
@@ -17,51 +17,7 @@
             </div>
 
         </li>
-        <li>The Incredible Hulk (2008)</li>
-        <li>Iron Man 2 (2010)</li>
-        <li>Thor (2011)</li>
-        <li>Captain America: The First Avenger (2011)</li>
-        <li>The Avengers (2012)</li>
-
-        <!-- Phase Two -->
-        <li>Iron Man 3 (2013)</li>
-        <li>Thor: The Dark World (2013)</li>
-        <li>Captain America: The Winter Soldier (2014)</li>
-        <li>Guardians of the Galaxy (2014)</li>
-        <li>Avengers: Age of Ultron (2015)</li>
-        <li>Ant-Man (2015)</li>
-
-        <!-- Phase Three -->
-        <li>Captain America: Civil War (2016)</li>
-        <li>Doctor Strange (2016)</li>
-        <li>Guardians of the Galaxy Vol. 2 (2017)</li>
-        <li>Spider-Man: Homecoming (2017)</li>
-        <li>Thor: Ragnarok (2017)</li>
-        <li>Black Panther (2018)</li>
-        <li>Avengers: Infinity War (2018)</li>
-        <li>Ant-Man and The Wasp (2018)</li>
-        <li>Captain Marvel (2019)</li>
-        <li>Avengers: Endgame (2019)</li>
-        <li>Spider-Man: Far From Home (2019)</li>
-
-        <!-- Phase Four -->
-        <li>Black Widow (2021)</li>
-        <li>Shang-Chi and the Legend of the Ten Rings (2021)</li>
-        <li>Eternals (2021)</li>
-        <li>Spider-Man: No Way Home (2021)</li>
-        <li>Doctor Strange in the Multiverse of Madness (2022)</li>
-        <li>Thor: Love and Thunder (2022)</li>
-        <li>Black Panther: Wakanda Forever (2022)</li>
-
-        <!-- Marvel Studios TV Shows -->
-        <li>WandaVision (2021)</li>
-        <li>The Falcon and The Winter Soldier (2021)</li>
-        <li>Loki (2021–present)</li>
-        <li>What If...? (2021–present)</li>
-        <li>Hawkeye (2021)</li>
-        <li>Moon Knight (2022)</li>
-        <li>Ms. Marvel (2022)</li>
-        <li>She-Hulk: Attorney at Law (2022)</li> 
+        
     </div>
 </template>
 <script>
